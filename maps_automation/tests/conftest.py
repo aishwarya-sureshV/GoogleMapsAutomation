b8c5721226e6364c472e32b9b59bd7a860ce3fdb
aishwarya-sureshV <aishwarya.sv25@gmail.com>
2025-11-14 03:56:41 +0530

    comments for Chrome driver fixture

--- maps_automation/tests/conftest.py
+++ maps_automation/tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from maps_automation.core.driver_factory import create_driver
 
+#this fixture will take care of setup & teardown process of chrome driver
 @pytest.fixture(scope="session")
 def driver():
     driver = create_driver(headless=False)
